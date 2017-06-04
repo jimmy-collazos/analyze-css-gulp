17d0f5a3bf2141a643c8110a2a6d9216177c3816
Jimmy Collazos <jimmy.collazos@gmail.com>
2017-06-04 16:08:32 +0200

    refactor

--- index.js
+++ index.js
@@ -1,60 +1,22 @@
-var through = require('through2'),
-  gutil = require('gulp-util'),
-  analyzer = require('analyze-css'),
-  fs = require('fs'),
-  path = require('path');
+ const through = require('through2');
+ const gutil = require('gulp-util');
+ const analyzer = require('./lib/analyzer');
+ const utils = require('./lib/utils');
 
-var metricFilterMap = function(offender) {
-  var position = offender.position && offender.position.start;
-  return offender.message + (position ? ' @ ' + position.line + ':' + position.column : '');
-};
-var metricFilter = function(metricName) {
-  this.offenders[metricName] = this.offenders[metricName].map(metricFilterMap);
-};
 
-module.exports = function (options) {
-  'use strict';
-  if (typeof options !== 'undefined' && Object.prototype.toString.call(options) !== '[object Object]') {
+module.exports = (options = {}) => {
+  if (!utils.isObject(options)) {
     throw new gutil.PluginError('analyze-css', 'Options is not a Object');
   }
-  options = options || {};
+  // options = options || {};
   function analizeCss(file, enc, callback) {
-    var _analyzeAndChangeContent = function(err, result) {
-        file.contents = new Buffer(JSON.stringify(result, null, 2));
-        callback(null, file);
-      },
-      _saveErrorCallback = function(err) {
-        if(err) {
-          throw err;
-        }
-      },
-      _analyzeAndSaveReport = function(err, result) {
-        var filename = path.basename(file.path, '.css'),
-          filepath = path.join(options.outDiretory, filename + '.json');
-
-        if (typeof result.offenders !== 'undefined') {
-          Object.keys(result.offenders).forEach(metricFilter, result);
-        }
-        fs.writeFile(filepath, JSON.stringify(result, null, 2), _saveErrorCallback);
-        callback(null, file);
-      };
-
-    // Do nothing if no contents
+    // Don't do nothing if don't have contents
     if (file.isNull()) {
-      this.push(file);
-      return callback();
-    }
-
-    if (file.isStream()) {
-      this.emit('error',
-        new gutil.PluginError('analyze-css', 'Stream content is not supported'));
-      return callback();
-    }
-
-    if (file.isBuffer()) {
-      new analyzer(String(file.contents), options, options.outDiretory ? _analyzeAndSaveReport : _analyzeAndChangeContent);
-    } else {
-      return callback();
+      cb(null, file);
+    } else if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('analyze-css', 'Stream content is not supported'));
+    } if (file.isBuffer()) {
+      analyzer(file, options, callback)
     }
   }
 
